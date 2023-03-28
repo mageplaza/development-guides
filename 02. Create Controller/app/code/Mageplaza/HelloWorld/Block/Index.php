ba03cabf25d8b64c725ee7b3811ce301740b57ef
chuccv <chuccv@mageplaza.com>
2023-03-28 16:08:40 +0700

    - How to Create Controller in Magento 2

---
+++ 02. Create Controller/app/code/Mageplaza/HelloWorld/Block/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace Mageplaza\HelloWorld\Block;
+
+/**
+ * Class Index
+ * @package Mageplaza\HelloWorld\Block
+ */
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ 02. Create Controller/app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -0,0 +1,31 @@
+<?php
+namespace Mageplaza\HelloWorld\Controller\Index;
+
+/**
+ * Class Index
+ * @package Mageplaza\HelloWorld\Controller\Index
+ */
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    ) {
+        $this->_pageFactory = $pageFactory;
+
+        return parent::__construct($context);
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        return $this->_pageFactory->create();
+    }
+}
